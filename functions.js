7f1a0286bea02b79c05626ad3918c5eccb77c719
Siya021 <siyamthandamazokwan@gmail.com>
2025-11-05 13:51:42 +0200

    Add functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet (name){
+return 'Hello, {name}';
+}
+
+function add(num1,num2){
+    return num1+num2
+}
+
+let greetingMessage = greet("Sibahle")
+let sum = add(5, 10)
+
+console.log(greetingMessage);
+console.log("The sum is:", sum);
